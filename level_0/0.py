391e5a0e4c7058c1198b5f0e063b2a7b36b2886a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-31 00:11:41 +0000

    Write a script or a program that votes 1024 times for your

--- level_0/0.py
+++ level_0/0.py
@@ -9,11 +9,11 @@ url = "http://158.69.76.135/level0.php"
 
 
 data_post = {
-    "id": "1528",
+    "id": "1595",
     "holdthedoor": "Submit+Query",
 }
 
-for i in range(10):
+for i in range(1024):
     requests.post(url, data=data_post, headers=header)
     print("Vote: {:d} for {:s}".format(i + 1, data_post["id"]))
-print("Fin de votación....")
+print("end of voting....")
